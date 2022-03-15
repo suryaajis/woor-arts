76d173395788de11b090f1d502f3fd38be0d0243
suryaajis330 <suryaajis330@gmail.com>
2022-03-15 07:17:02 +0700

    responsive sidenav

--- client/src/components/Sidenav.js
+++ client/src/components/Sidenav.js
@@ -1,42 +1,136 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SideNav = () => {
+const Sidenav = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div class="w-60 h-full shadow-md bg-white px-1 absolute">
-      <ul class="relative">
-        <li class="relative">
-          <a
-            class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
-            href="#!"
-            data-mdb-ripple="true"
-            data-mdb-ripple-color="dark"
+    <>
+      <div className="bg-gray-800 text-gray-100 flex justify-between md:hidden">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="left-0 mobile-menu-button p-4 focus:outline-none focus:bg-gray-700"
+          aria-controls="mobile-menu"
+          aria-expanded="false"
+          type="button"
+        >
+          <span className="sr-only">Open main menu</span>
+          {!isOpen ? (
+            <svg
+              className="block h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          ) : (
+            <svg
+              className="block h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
+
+      <div className="sidebar bg-gray-800 text-blue-100 w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out z-0">
+        <a href="#" className="text-white flex items-center space-x-2 px-4">
+          <svg
+            className="w-8 h-8"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
           >
-            Sidenav link 1
-          </a>
-        </li>
-        <li class="relative">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
+            />
+          </svg>
+          <span className="text-2xl font-extrabold">Woor Arts</span>
+        </a>
+
+        <nav>
           <a
-            class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
-            href="#!"
-            data-mdb-ripple="true"
-            data-mdb-ripple-color="dark"
+            href="#"
+            className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white"
           >
-            Sidenav link 2
+            About Me
           </a>
-        </li>
-        <li class="relative">
           <a
-            class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
-            href="#!"
-            data-mdb-ripple="true"
-            data-mdb-ripple-color="dark"
+            href=""
+            className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white"
           >
-            Sidenav link 2
+            Bussiness and Commercial
           </a>
-        </li>
-      </ul>
-    </div>
+          <a
+            href=""
+            className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white"
+          >
+            Social Media
+          </a>
+          <a
+            href=""
+            className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white"
+          >
+            Check and Check
+          </a>
+        </nav>
+      </div>
+
+      {isOpen ? (
+        <></>
+      ) : (
+        <div className="md:hidden" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <a
+              href="#"
+              className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white"
+            >
+              About Me
+            </a>
+            <a
+              href=""
+              className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white"
+            >
+              Bussiness and Commercial
+            </a>
+            <a
+              href=""
+              className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white"
+            >
+              Social Media
+            </a>
+            <a
+              href=""
+              className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white"
+            >
+              Check and Check
+            </a>
+          </div>
+          ;
+        </div>
+      )}
+    </>
   );
 };
 
-export default SideNav
+export default Sidenav;
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,19 +1,28 @@
 import React from "react";
 import SideNav from "../components/Sidenav";
-import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    <div>
+    <div className="min-h-screen md:flex">
       <SideNav />
-      <Navbar />
-      <p className="italic text-3xl font-bold">Hello World From The Hell</p>;
-      <img
-        src="https://drive.google.com/uc?id=1HjN7oTxY1eZbAa45anndbbB4wtXJvEiH"
-        style={{ width: 400, height: 400 }}
-      />
+
+      <div className="ml-5 mt-5">
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          <a className="group">
+            <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-sm overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+              <div
+                className="w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 cursor-pointer"
+              >
+                Trippy
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+// https://drive.google.com/uc?id=1HjN7oTxY1eZbAa45anndbbB4wtXJvEiH
